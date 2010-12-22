b5ad88bf656c6696d6ab72739d833bc9ed9c1079
Taro Funaki <taro_funaki@spicysoft.com>
2010-12-22 16:17:14 +0900

    first commit

---
+++ library/SQL/Maker.php
@@ -0,0 +1,529 @@
+<?php
+namespace SQL;
+
+class Maker {
+    const DEFAULT_ALIAS = 'me';
+
+    private $logic;
+    private $aliases;
+
+    public function __construct () {
+        $this->_setLogic( 'AND' );
+        $this->aliases = array();
+    }
+
+    private function _setLogic ( $logic ) {
+        $this->logic = sprintf( ' %s ', strtoupper( $logic ) );
+    }
+
+    private static function _quote ( $str ) {
+        return "`$str`";
+    }
+
+    private function _addAlias ( $column ) {
+        if ( in_array( $column, $this->aliases ) ) {
+            return $column;
+        }
+
+        return sprintf(
+            '`%s`.%s',
+            self::DEFAULT_ALIAS,
+            $column
+        );
+    }
+
+    private function _insert ( $data ) {
+        switch ( self::getType( $data ) ) {
+            case 'hash':
+                return $this->_insertFromHash( $data );
+            default:
+                throw new \Exception( 'Unknown type' );
+        }
+    }
+
+    private function _insertFromHash ( $data ) {
+        $sql = sprintf(
+            '( %s ) VALUES ( %s )',
+            implode( ',', array_map( 'self::_quote', array_keys( $data ) ) ),
+            implode( ',', array_fill( 0, count( $data ), '?' ) )
+        );
+        $params = array_values( $data );
+
+        return array( $sql, $params );
+    }
+
+    public function insert ( $table, $data ) {
+        $table = self::_quote( $table );
+        list( $sql, $params ) = $this->_insert( $data );
+        $sql = implode( ' ', array( 'INSERT INTO', $table, $sql ) );
+
+        return array( $sql, $params );
+    }
+
+    private function _update ( $data ) {
+        switch ( self::getType( $data ) ) {
+            case 'hash':
+                return $this->_updateFromHash( $data );
+            default:
+                throw new \Exception( 'Unknown type' );
+        }
+    }
+
+    private function _updateFromHash ( $data ) {
+        $set    = array();
+        $params = array();
+        foreach ( $data as $column => $value ) {
+            $column = self::_quote( $column );
+            array_push( $set, sprintf( '%s = ?', $column ) );
+            array_push( $params, $value );
+        }
+
+        $sql = implode( ',', $set );
+
+        return array( $sql, $params );
+    }
+
+    public function update ( $table, $data, $where = array(), $attrs = array() ) {
+        $table = self::_quote( $table );
+        list( $sql, $params ) = $this->_update( $data );
+        $sql = implode( ' ', array( 'UPDATE', $table, 'SET', $sql ) );
+
+        if ( count( $where ) ) {
+            list( $whereSql, $whereParams ) = $this->where( $where, $attrs );
+            $sql .= $whereSql;
+            $params = array_merge( $params, $whereParams );
+        }
+
+        return array( $sql, $params );
+    }
+
+    private function _fieldsFromHash ( $fields ) {
+        $func = '';
+        $as   = '';
+        foreach ( $fields as $key => $value ) {
+            if ( preg_match( '/^-(as)/i', $key, $matches ) ) {
+                $alias = self::_quote( $value );
+                array_push( $this->aliases, $alias );
+                $as = sprintf(
+                    ' %s %s',
+                    strtoupper( $matches[1] ),
+                    $alias
+                );
+                continue;
+            }
+
+            $str = $this->_recurseFields( $value );
+            if ( preg_match( '/^ AS /', $str ) ) {
+                $as = $str;
+
+                if ( preg_match( '/[\(\)\*\.]/', $key ) ) {
+                    $func = $key;
+                    continue;
+                }
+
+                $func = $this->_addAlias( self::_quote( $key ) );
+                continue;
+            }
+
+            $func = sprintf(
+                '%s( %s )',
+                strtoupper( $key ),
+                $str
+            );
+        }
+        return "$func$as";
+    }
+
+    private function _recurseFields ( $fields ) {
+        switch ( self::getType( $fields ) ) {
+            case 'string':
+                if ( preg_match( '/[\(\)\*\.]/', $fields ) ) {
+                    return $fields;
+                }
+                return $this->_addAlias( self::_quote( $fields ) );
+            case 'array':
+                $f = array();
+                foreach ( $fields as $field ) {
+                    array_push( $f, $this->_recurseFields( $field ) );
+                }
+                return implode( ',', $f );
+            case 'hash':
+                return $this->_fieldsFromHash( $fields );
+            default:
+                throw new \Exception( 'Unknown type' );
+        }
+    }
+
+    private function _joinFromHash ( $join ) {
+        $table = '';
+        $cond  = '';
+        foreach ( $join as $key => $value ) {
+            if ( preg_match( '/^-(inner|left)/i', $key, $matches ) ) {
+                $table = sprintf(
+                    ' %s JOIN %s',
+                    strtoupper( $matches[1] ),
+                    $this->_table( $value )
+                );
+            }
+
+            if ( $key === '-on' ) {
+                $cond = sprintf(
+                    'ON %s = %s',
+                    $this->_addAlias( self::_quote( array_shift( $value ) ) ),
+                    array_shift( $value )
+                );
+            }
+        }
+        return "$table $cond";
+    }
+
+    private function _join ( $join ) {
+        switch ( self::getType( $join ) ) {
+            case 'array':
+                $sql = '';
+                foreach ( $join as $j ) {
+                    $sql .= $this->_joinFromHash( $j );
+                }
+                return $sql;
+            case 'hash':
+                return $this->_joinFromHash( $join );
+            default:
+                throw new \Exception( 'Unknown type' );
+        }
+    }
+
+    private function _table ( $table ) {
+        switch ( self::getType( $table ) ) {
+            case 'string':
+                return sprintf(
+                    '%s %s',
+                    self::_quote( $table ),
+                    self::_quote( self::DEFAULT_ALIAS )
+                );
+            case 'array':
+                list( $entity, $alias ) = $table;
+                return sprintf(
+                    '%s %s',
+                    self::_quote( $entity ),
+                    self::_quote( $alias )
+                );
+            case 'hash':
+                reset( $table );
+                list( $key, $value ) = each( $table );
+                $base = $this->_table( $key );
+                $join = $this->_join( $value );
+                return "$base$join";
+            default:
+                throw new \Exception( 'Unknown type' );
+        }
+    }
+
+    public function select ( $table, $fields = '*', $where = array(), $attrs = array() ) {
+        $table = $this->_table( $table );
+
+        $fields = $this->_recurseFields( $fields );
+
+        $sql    = implode( ' ', array( 'SELECT', $fields, 'FROM', $table ) );
+        $params = array();
+
+        if ( count( $where ) || count( $attrs ) ) {
+            list( $whereSql, $whereParams ) = $this->where( $where, $attrs, true );
+            $sql .= $whereSql;
+            $params = $whereParams;
+        }
+
+        array_splice( $this->aliases, 0 );
+        return array( $sql, $params );
+    }
+
+    public function delete ( $table, $where = array(), $attrs = array() ) {
+        $table  = self::_quote( $table );
+        $sql    = implode( ' ', array( 'DELETE', 'FROM', $table ) );
+        $params = array();
+
+        if ( count( $where ) ) {
+            list( $whereSql, $whereParams ) = $this->where( $where, $attrs );
+            $sql .= $whereSql;
+            $params = $whereParams;
+        }
+
+        return array( $sql, $params );
+    }
+
+    public function replace ( $table, $data ) {
+        $table = self::_quote( $table );
+        list( $sql, $params ) = $this->_insert( $data );
+        $sql = implode( ' ', array( 'REPLACE INTO', $table, $sql ) );
+
+        return array( $sql, $params );
+    }
+
+    private function _recurseWhere ( $where, $alias ) {
+        switch ( self::getType( $where ) ) {
+            case 'array':
+                return $this->_whereFromArray( $where, $alias );
+            case 'hash':
+                return $this->_whereFromHash( $where, $alias );
+            default:
+                throw new \Exception( 'Unknown type' );
+        }
+    }
+
+    private function _whereFromArray ( $where, $alias ) {
+        $clauses = array();
+        $params  = array();
+        foreach ( $where as $element ) {
+            switch ( self::getType( $element ) ) {
+                case 'string':
+                    $column = $element;
+                    if ( ! preg_match( '/[`\.]/', $column ) ) {
+                        $column = self::_quote( $column );
+                        if ( $alias ) {
+                            $column = $this->_addAlias( $column );
+                        }
+                    }
+
+                    $op     = '=';
+                    array_push( $clauses, sprintf( '%s %s ?', $column, $op ) );
+                    break;
+                case 'hash':
+                    list( $sql, $parameters ) = $this->_whereFromHash( $element, $alias );
+                    array_push( $clauses, $sql );
+                    $params = array_merge( $params, $parameters );
+                    break;
+                default:
+                    throw new \Exception( 'Unknown type' );
+            }
+        }
+
+        $sql = implode( $this->logic, $clauses );
+        if ( $sql ) {
+            $sql = "( $sql )";
+        }
+
+        return array( $sql, $params );
+    }
+
+    private function _whereFromHash ( $where, $alias ) {
+        $clauses = array();
+        $params  = array();
+        foreach ( $where as $key => $value ) {
+            $op     = '=';
+            $holder = '?';
+            switch ( self::getType( $value ) ) {
+                case 'integer':
+                    array_push( $params, $value );
+                    break;
+                case 'string':
+                    array_push( $params, $value );
+                    break;
+                case 'array':
+                    if ( preg_match( '/^-(and|or)/i', $key, $matches ) ) {
+                        $logic = $this->logic;
+                        $this->_setLogic( $matches[1] );
+                        list( $sql, $parameters ) = $this->_whereFromArray( $value, $alias );
+                        array_push( $clauses, $sql );
+                        $params = array_merge( $params, $parameters );
+                        $this->logic = $logic;
+                    }
+                    break;
+                case 'hash':
+                    if ( count( $value ) > 1 ) {
+                        throw new \Exception( 'Unknown type' );
+                    }
+                    list( $op ) = array_keys( $value );
+                    $p = $value[$op];
+                    if ( preg_match( '/^-(in)/i', $op, $matches ) ) {
+                        $params = array_merge( $params, $p );
+
+                        $op     = strtoupper( $matches[1] );
+                        $holder = implode( ',', array_fill( 0, count( $p ), '?' ) );
+                        $holder = "( $holder )";
+                        break;
+                    }
+                    array_push( $params, $p );
+                    break;
+                default:
+                    throw new \Exception( 'Unknown type' );
+            }
+
+            if ( ! preg_match( '/^-/', $key ) ) {
+                $column = $key;
+                if ( ! preg_match( '/[`\.]/', $column ) ) {
+                    $column = self::_quote( $column );
+                    if ( $alias ) {
+                        $column = $this->_addAlias( $column );
+                    }
+                }
+                array_push( $clauses, sprintf( '%s %s %s', $column, $op, $holder ) );
+            }
+        }
+
+        $sql = implode( $this->logic, $clauses );
+        if ( $sql ) {
+            $sql = "( $sql )";
+        }
+
+        return array( $sql, $params );
+    }
+
+    private function _groupByChunks ( $group ) {
+        $sql = array();
+
+        switch ( self::getType( $group ) ) {
+            case 'string':
+                if ( ! preg_match( '/[`\.]/', $group ) ) {
+                    $group = $this->_addAlias( self::_quote( $group ) );
+                }
+                array_push( $sql, $group );
+                break;
+            case 'array':
+                foreach ( $group as $g ) {
+                    $sql = array_merge(
+                        $sql,
+                        $this->_groupByChunks( $g )
+                    );
+                }
+                break;
+            default:
+                throw new \Exception( 'Unknown type' );
+        }
+
+        return $sql;
+    }
+
+    private function _orderByChunks ( $order ) {
+        $sql = array();
+
+        switch ( self::getType( $order ) ) {
+            case 'string':
+                if ( ! preg_match( '/[`\.]/', $order ) ) {
+                    $order = $this->_addAlias( self::_quote( $order ) );
+                }
+                array_push( $sql, $order );
+                break;
+            case 'array':
+                foreach ( $order as $o ) {
+                    $sql = array_merge(
+                        $sql,
+                        $this->_orderByChunks( $o )
+                    );
+                }
+                break;
+            case 'hash':
+                if ( count( $order ) > 1 ) {
+                    throw new \Exception( 'Unknown type' );
+                }
+                list( $key ) = array_keys( $order );
+                if ( preg_match( '/^-(asc|desc)/i', $key, $matches ) ) {
+                    $ordering = $this->_orderByChunks( $order[$key] );
+                    array_push(
+                        $sql,
+                        sprintf(
+                            '%s %s',
+                            array_shift( $ordering ),
+                            strtoupper( $matches[1] )
+                        )
+                    );
+                }
+                break;
+            default:
+                throw new \Exception( 'Unknown type' );
+        }
+
+        return $sql;
+    }
+
+    private function _groupBy ( $group ) {
+        $sql = $this->_groupByChunks( $group );
+
+        return sprintf(
+            ' GROUP BY %s',
+            implode( ',', $sql )
+        );
+    }
+
+    private function _orderBy ( $order ) {
+        $sql = $this->_orderByChunks( $order );
+
+        return sprintf(
+            ' ORDER BY %s',
+            implode( ',', $sql )
+        );
+    }
+
+    public function where ( $where, $attrs = array(), $alias = false ) {
+        list( $sql, $params ) = $this->_recurseWhere( $where, $alias );
+        if ( $sql ) {
+            $sql = " WHERE $sql";
+        }
+
+        if ( isset( $attrs['groupBy'] ) ) {
+            $sql .= $this->_groupBy( $attrs['groupBy'] );
+        }
+
+        if ( isset( $attrs['orderBy'] ) ) {
+            $sql .= $this->_orderBy( $attrs['orderBy'] );
+        }
+
+        if ( isset( $attrs['rows'] ) ) {
+            $sql .= $this->_limit( $attrs['rows'] );
+        }
+
+        if ( isset( $attrs['offset'] ) ) {
+            $sql .= $this->_offset( $attrs['offset'] );
+        }
+        return array( $sql, $params );
+
+    }
+
+    private function _limit ( $rows ) {
+        switch ( self::getType( $rows ) ) {
+            case 'integer':
+                return sprintf( ' LIMIT %d', $rows );
+                break;
+            case 'string':
+                return sprintf( ' LIMIT %d', $rows );
+                break;
+            default:
+                throw new \Exception( 'Unknown type' );
+        }
+    }
+
+    private function _offset ( $num ) {
+        switch ( self::getType( $num ) ) {
+            case 'integer':
+                return sprintf( ' OFFSET %d', $num );
+                break;
+            default:
+                throw new \Exception( 'Unknown type' );
+        }
+    }
+
+    private static function getType ( &$stuff ) {
+        $type = gettype( $stuff );
+
+        if ( $type !== 'array' ) {
+            return $type;
+        }
+
+        if ( self::isHash( $stuff ) ) {
+            return 'hash';
+        }
+        return 'array';
+    }
+
+    private static function isHash ( &$array ) {
+        if ( ! is_array( $array ) ) {
+            return false;
+        }
+
+        $i = 0;
+        foreach( $array as $key => $v ) {
+            if ( $key !== $i++ ) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ tests/SQLMakerTest.php
@@ -0,0 +1,272 @@
+<?php
+namespace Test;
+
+$path = array(
+    __DIR__ . '/../library',
+    get_include_path()
+);
+set_include_path( implode( PATH_SEPARATOR, $path ) );
+
+require_once 'SQL/Maker.php';
+
+/**
+ * Test class for SQL\Maker.
+ */
+class SQLMakerTest extends \PHPUnit_Framework_TestCase
+{
+    protected $object;
+    protected function setUp()
+    {
+        $this->object = new \SQL\Maker;
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    public function testInsert()
+    {
+        list( $sql, $params ) = $this->object->insert(
+            'user',
+            array( 'id' => 1, 'name' => 'sample' )
+        );
+
+        $this->assertEquals(
+            'INSERT INTO `user` ( `id`,`name` ) VALUES ( ?,? )',
+            $sql
+        );
+
+        $this->assertEquals( array( 1, 'sample' ), $params );
+    }
+
+    public function testUpdate()
+    {
+        list( $sql1, $params1 ) = $this->object->update(
+            'user',
+            array( 'name' => 'dummy', 'email' => 'dummy@sample.com' )
+        );
+
+        $this->assertEquals(
+            'UPDATE `user` SET `name` = ?,`email` = ?',
+            $sql1
+        );
+
+        $this->assertEquals( array( 'dummy', 'dummy@sample.com' ), $params1 );
+    }
+
+    public function testSelect()
+    {
+        list( $sql1, $params1 ) = $this->object->select( 'user' );
+        list( $sql2, $params2 ) = $this->object->select( 'user', array( 'id', 'name' ) );
+        list( $sql3 ) = $this->object->select(
+            'employee',
+            array(
+                'name',
+                array( 'count' => 'employee_id' ),
+                array( 'max' => array( 'length' => 'name' ), '-as' => 'longest_name' )
+            )
+        );
+        list( $sql4 ) = $this->object->select(
+            array(
+                'user' => array(
+                    '-inner' => array( 'item', 'i' ),
+                    '-on'    => array( 'id', '`i`.`user_id`' )
+                )
+            )
+        );
+
+        list( $sql5 ) = $this->object->select(
+            array(
+                'user' => array(
+                    array(
+                        '-inner' => array( 'item', 'i' ),
+                        '-on'    => array( 'id', '`i`.`user_id`' )
+                    ),
+                    array(
+                        '-inner' => array( 'collection', 'c' ),
+                        '-on'    => array( 'id', '`c`.`id`' )
+                    )
+                )
+            )
+        );
+
+        list( $sql6 ) = $this->object->select(
+            'employee',
+            array(
+                'name' => array( '-as' => 'alias' )
+            )
+        );
+
+        $this->assertEquals(
+            'SELECT * FROM `user` `me`',
+            $sql1
+        );
+
+        $this->assertEquals( array(), $params1 );
+
+        $this->assertEquals(
+            'SELECT `me`.`id`,`me`.`name` FROM `user` `me`',
+            $sql2
+        );
+
+        $this->assertEquals( array(), $params2 );
+
+        $this->assertEquals(
+            'SELECT `me`.`name`,COUNT( `me`.`employee_id` ),MAX( LENGTH( `me`.`name` ) ) AS `longest_name` FROM `employee` `me`',
+            $sql3
+        );
+
+        $this->assertEquals(
+            'SELECT * FROM `user` `me` INNER JOIN `item` `i` ON `me`.`id` = `i`.`user_id`',
+            $sql4
+        );
+
+        $this->assertEquals(
+            'SELECT * FROM `user` `me` INNER JOIN `item` `i` ON `me`.`id` = `i`.`user_id` INNER JOIN `collection` `c` ON `me`.`id` = `c`.`id`',
+            $sql5
+        );
+
+        $this->assertEquals(
+            'SELECT `me`.`name` AS `alias` FROM `employee` `me`',
+            $sql6
+        );
+    }
+
+    public function testDelete()
+    {
+        list( $sql1, $params1 ) = $this->object->delete( 'user' );
+
+        $this->assertEquals(
+            'DELETE FROM `user`',
+            $sql1
+        );
+
+        $this->assertEquals( array(), $params1 );
+    }
+
+    public function testReplace()
+    {
+        list( $sql, $params ) = $this->object->replace(
+            'user',
+            array( 'id' => 1, 'name' => 'sample' )
+        );
+
+        $this->assertEquals(
+            'REPLACE INTO `user` ( `id`,`name` ) VALUES ( ?,? )',
+            $sql
+        );
+
+        $this->assertEquals( array( 1, 'sample' ), $params );
+    }
+
+    public function testWhere()
+    {
+        list( $sql1, $params1 ) = $this->object->where(
+            array(
+                'id'    => array( '<' => 100 ),
+                'name'  => 'dummy',
+                'email' => 'dummy@sample.com'
+            )
+        );
+        list( $sql2, $params2 ) = $this->object->where(
+            array( 'id', 'name', 'email' )
+        );
+        list( $sql3 ) = $this->object->where(
+            array(),
+            array( 'orderBy' => array(
+                'id',
+                array( '-desc' => 'name' )
+            ) )
+        );
+        list( $sql4 ) = $this->object->where(
+            array(),
+            array( 'rows' => 5 )
+        );
+        list( $sql5 ) = $this->object->where(
+            array(),
+            array(
+                'rows'   => 5,
+                'offset' => 10
+            )
+        );
+        list( $sql6, $params6 ) = $this->object->where(
+            array( '-or' => array(
+                array( 'source_id' => 1 ),
+                array( 'dest_id' => 1 )
+            ) )
+        );
+        list( $sql7 ) = $this->object->where(
+            array(),
+            array(
+                'groupBy' => array( 'book', 'chapter' )
+            )
+        );
+        list( $sql8 ) = $this->object->where(
+            array(),
+            array( 'orderBy' => array(
+                '`foreign`.`id`',
+                array( '-desc' => '`foreign`.`name`' )
+            ) )
+        );
+        list( $sql9, $params9 ) = $this->object->where(
+            array( 'id'=> array( '-in' => array( 1, 2, 3 ) ) )
+        );
+
+        $this->assertEquals(
+            ' WHERE ( `id` < ? AND `name` = ? AND `email` = ? )',
+            $sql1
+        );
+
+        $this->assertEquals( array( 100, 'dummy', 'dummy@sample.com' ), $params1 );
+
+        $this->assertEquals(
+            ' WHERE ( `id` = ? AND `name` = ? AND `email` = ? )',
+            $sql2
+        );
+
+        $this->assertEquals( array(), $params2 );
+
+        $this->assertEquals(
+            ' ORDER BY `me`.`id`,`me`.`name` DESC',
+            $sql3
+        );
+
+        $this->assertEquals(
+            ' LIMIT 5',
+            $sql4
+        );
+
+        $this->assertEquals(
+            ' LIMIT 5 OFFSET 10',
+            $sql5
+        );
+
+        $this->assertEquals(
+            ' WHERE ( ( ( `source_id` = ? ) OR ( `dest_id` = ? ) ) )',
+            $sql6
+        );
+
+        $this->assertEquals(
+            array( 1, 1 ),
+            $params6
+        );
+
+        $this->assertEquals(
+            ' GROUP BY `me`.`book`,`me`.`chapter`',
+            $sql7
+        );
+
+        $this->assertEquals(
+            ' ORDER BY `foreign`.`id`,`foreign`.`name` DESC',
+            $sql8
+        );
+
+        $this->assertEquals(
+            ' WHERE ( `id` IN ( ?,?,? ) )',
+            $sql9
+        );
+
+        $this->assertEquals( array( 1, 2, 3 ), $params9 );
+
+    }
+}
